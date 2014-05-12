f55d86e09d5352f3678dfdec47aa0c27c170a0b6
Mike Carlton <mike@carltons.us>
2014-05-12 09:50:13 -0700

    add name metadata (to satisfy ridley)

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "Coroutine LLC"
 maintainer_email "bmontgomery@coroutine.com"
 license          "Apache 2.0"
+name             "system_packages"
 description      "Easily install arbitrary system packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
